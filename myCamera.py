e8a8bdb9b69eb9c87c1179b7cfc861123df0e383
Ulfu <din_dr_elvis@live.se>
2017-02-21 14:26:24 +0100

    Update myCamera.py

--- myCamera.py
+++ myCamera.py
@@ -1,13 +1,13 @@
 from picamera import PiCamera
 
 class cameraClass:
-	def __init__(self, resolutionX, resolutionY):
+	def __init__(self, resolutionX, resolutionY):#setup camera
 		self.resolutionX = resolutionX
         self.resolutionY = resolutionY
         self.camera = PiCamera()
         self.camera.resolution = (resolutionX, resolutionY)
         
-	def capture(self, name):
+	def capture(self, name):#capture image
         camera.start_preview()
         camera.capture(name,'.png')
 		camera.close()
