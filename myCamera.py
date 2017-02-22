e420479d9f9b931a51330b7d160b6235ebf4dfba
Ulfu <din_dr_elvis@live.se>
2017-02-22 16:32:49 +0100

    Fixed code alignment

--- myCamera.py
+++ myCamera.py
@@ -2,22 +2,22 @@ from picamera import PiCamera
 
 class cameraClass:
 	def __init__(self, resolutionX, resolutionY):#setup camera
-        self.resolutionX = resolutionX
-        self.resolutionY = resolutionY
-        self.camera = PiCamera()
-        self.camera.resolution = (resolutionX, resolutionY)
+                self.resolutionX = resolutionX
+                self.resolutionY = resolutionY
+                self.camera = PiCamera()
+                self.camera.resolution = (resolutionX, resolutionY)
         
 	def capture(self, name):#capture image
-        name = name + '.png'
-        camera.start_preview()
-        camera.capture(name)
-        camera.close()
+                name = name + '.png'
+                #camera.start_preview()
+                camera.capture(name)
+                #camera.close()
 
-    def convertToBinary(self, name):
-        name = name + '.png'
-        with open(name, 'rb') as imageFile:
-        file = imageFile.read()
-        b = bytearray(file)
-		return b
-        #print b[0]
+        def convertToBinary(self, name):
+                name = name + '.png'
+                with open(name, 'rb') as imageFile:
+                file = imageFile.read()
+                b = bytearray(file)
+                return b
+                #print b[0]
 
