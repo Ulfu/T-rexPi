4471878d3f12898d4b11c2edf0f35959ee1f1053
Ulfu <ulfu>
2017-04-18 01:39:33 +0200

    Removed unused code

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 import _thread
 import sys
 import motorControl
-#import servoControl
-import time#
 import mySocket
 import myCamera
 
 HOST = ''
-def runLidar(host):
-    print('Run Lidar ')
-    #setupLidar()
-    processing = mySocket.serverSocket(host, 6511)
-    processing.connect()    #Host , port
-    while True:
-        distances = servoControl.sweepRight()    #distances is an array
-        processing.sendData(distances)
-        distances = servoControl.sweepLeft()
-        processing.sendData(distances)
-        #pause
     
 def runMotors(host):
     print('Run Motors')
@@ -33,7 +20,6 @@ def runMotors(host):
                 steeringThrust = app.getData(data, 8, 16)
                 motorControl.steeringThrust(thrust, steeringThrust)
                 #print(thrust , ' ', steeringThrust) #For debugging
-                #time.sleep(0.01) #Try without the pause
             except app.timeOut:
                 print(app.timeOut)
                 break;
@@ -52,8 +38,6 @@ def cameraStream(host):
         finally:
             camera.end()
             cameraSocket.end()
-                
-_thread.start_new_thread(runLidar, (HOST,))
 
 _thread.start_new_thread(runMotors, (HOST,))
 
